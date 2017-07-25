5ad406b02e82b383ba25de874d796eea31f38509
Husseny <abdullrahman.elhusseni@gmail.com>
2017-07-25 15:49:52 +0200

    cr

--- betahash/static/betahash/js/homeApp.js
+++ betahash/static/betahash/js/homeApp.js
@@ -2,9 +2,10 @@ var app = angular.module('homeApp', []);
 
 app.config(function($interpolateProvider, $httpProvider) {
 	$interpolateProvider.startSymbol('{[{');
-    $interpolateProvider.endSymbol('}]}');
-    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+	$interpolateProvider.endSymbol('}]}');
+	$httpProvider.defaults.xsrfCookieName = 'csrftoken';
 	$httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+	$httpProvider.defaults.enctype="multipart/form-data";
 });
 
 app.factory('Scopes', function ($rootScope) {
